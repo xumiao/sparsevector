1e3de0a0d5697bf6fe12908c2d5aa37d4d5e3538
xu miao <miaoxu@microsoft.com>
2013-10-14 16:31:15 -0700

    add new sequential insertion tests

--- sparse_vector_tester.py
+++ sparse_vector_tester.py
@@ -4,14 +4,16 @@ Created on Sun Oct 13 23:27:25 2013
 
 @author: xm
 """
+import pyximport; pyximport.install(setup_args={"include_dirs":np.get_include()}, reload_support=True)
+
 import numpy as np
-import pyximport; pyximport.install(setup_args={"script_args":["--compiler=mingw32"], "include_dirs":np.get_include()}, reload_support=True)
 
 from sparse_vecotr import DenseArray, SparseArray                
 from sparse_block_vector import SparseBlockArray
 from sparse_skiplist_vector import SparseSkipList
 
 from timeit import timeit
+
         self.a = DenseArray(100000)
         self.b = DenseArray(100000)
         self.sa = SparseArray()
@@ -24,19 +26,29 @@ from timeit import timeit
 def testAdd(a, b, w):
     a.add(b, w)
 
-def testSequentialInsert(a, sparsity = 0.5, sz = 1000000):
+def testSequentialRandomInsert(a, sparsity = 0.5, sz = 1000000):
     def insertValue(x):
         v = np.random.rand()
         if v > sparsity:
             a[x] = v
     map(insertValue, range(sz))
 
+def testSequentialBlockInsert(a, blockSize = 1000, jumpRange = 100, sz = 1000):
+    def insertValues(x):
+        x += int(np.random.rand() * jumpRange)
+        for i in range(blockSize):
+            a[x+i] = np.random.rand()
+    map(insertValues, range(sz))
+    
 def testDotProduct(a, b):
     return a.dot(b)
 
 def testMemorySize(a):
     print a.memorySize()
         
+def experSequentialRandomInsert():
+    pass
 
-def experSequentialInsert():
+def experSequentialBlockInsert():
+    pass
     
