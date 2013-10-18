16c2e5b9551fd958ae2c62b401b83d3b5bd02402
xu miao <miaoxu@microsoft.com>
2013-10-18 09:16:49 -0700

    Update testing

--- sparse_vector_tester.py
+++ sparse_vector_tester.py
@@ -4,42 +4,37 @@ Created on Sun Oct 13 23:27:25 2013
 
 @author: xm
 """
-import pyximport; pyximport.install(setup_args={"include_dirs":np.get_include()}, reload_support=True)
-
 import numpy as np
 
-from sparse_vecotr import DenseArray, SparseArray                
-from sparse_block_vector import SparseBlockArray
-from sparse_skiplist_vector import SparseSkipList
+import pyximport; pyximport.install(setup_args={"include_dirs":np.get_include()}, reload_support=True)
+
+#from sparse_vecotr import DenseArray, SparseArray                
+#from sparse_block_vector import SparseBlockArray
+#from sparse_skiplist_vector import SparseSkipList
 
 from timeit import timeit
 
-        self.a = DenseArray(100000)
-        self.b = DenseArray(100000)
-        self.sa = SparseArray()
-        self.sb = SparseArray()
-        self.sba = SparseBlockArray(100)
-        self.sbb = SparseBlockArray(100)
-        self.ssa = SparseSkipList()
-        self.ssb = SparseSkipList()
-        
-def testAdd(a, b, w):
-    a.add(b, w)
-
-def testSequentialRandomInsert(a, sparsity = 0.5, sz = 1000000):
+      
+def generateRandomSparseData(sparsity = 0.5, sz = 1000000):
+    data = []
     def insertValue(x):
         v = np.random.rand()
         if v > sparsity:
-            a[x] = v
+            data.append((x,v))
     map(insertValue, range(sz))
+    return data
 
-def testSequentialBlockInsert(a, blockSize = 1000, jumpRange = 100, sz = 1000):
-    def insertValues(x):
-        x += int(np.random.rand() * jumpRange)
+def generateRandomBlockSparseData(blockSize = 1000, jumpRange = 100, sz = 1000):
+    data = []
+    def insertValues(x, y):
+        x += int(np.random.rand() * jumpRange) * blockSize
         for i in range(blockSize):
-            a[x+i] = np.random.rand()
-    map(insertValues, range(sz))
+            data.append(((x+i), np.random.rand()))
+    reduce(insertValues, range(sz), 0)
     
+def testAdd(a, b, w):
+    a.add(b, w)
+
 def testDotProduct(a, b):
     return a.dot(b)
 
@@ -47,7 +42,23 @@ def testMemorySize(a):
     print a.memorySize()
         
 def experSequentialRandomInsert():
-    pass
+    sz = 10
+    sparsity = 0.5
+#    b = DenseArray(sz)
+#    sa = SparseArray()
+#    sb = SparseArray()
+#    sba = SparseBlockArray(100)
+#    sbb = SparseBlockArray(100)
+#    ssa = SparseSkipList()
+#    ssb = SparseSkipList()
+    
+    denseResult = timeit("for key,value in data: a[key] = value", \
+    setup="from sparse_vector import DenseArray; \
+    import sparse_vector_tester as svt;\
+    sz = %d;\
+    a=DenseArray(sz);\
+    data = svt.generateRandomSparseData(sparsity=%f, sz=%d)"%(sz,sparsity,sz),\
+    number=100)
 
 def experSequentialBlockInsert():
     pass
